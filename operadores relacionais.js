29a2c9cee0c2edcf9a400d782549e75d39408f56
Felipe Braga <felipeabraga13@gmail.com>
2021-02-18 22:23:27 -0300

    removi todos os comentarios

--- operadores relacionais.js
+++ operadores relacionais.js
@@ -1,19 +1,4 @@
-/* 
-Var
-// Seção de Declarações das variáveis 
-   nota01, nota02: real
 
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   nota01 <- 10
-   nota02 <- 15
-   se(nota01 <> nota02) entao
-     escreval("Nota01 é diferente da nota02")
-   senao
-     escreval("nota01 é igual a nota02")
-   fimse
-Fimalgoritmo
-*/
 var nota01, nota02;
 nota01 = 10;
 nota02 = 10;
